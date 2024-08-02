7462098faa902159d06496d1c7352e4ff54bf1fd
baraabd <bara.abdullatif@outlook.com>
2024-08-02 20:42:16 +0200

    Using & Outputting Dynamic Values

--- EssentailReact/src/App.jsx
+++ EssentailReact/src/App.jsx
@@ -1,14 +1,30 @@
-function App() {
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+const genRandomInt = (max) => {
+return Math.floor(Math.random() * (max + 1))}
+
+
+
+const Header = () => {
+  const description = reactDescriptions[genRandomInt(reactDescriptions.length-1)]
+  
+  return (
+    <header>
+    <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+    <h1>React Essentials</h1>
+    <p>
+      {description} React concepts you will need for almost any app you are
+      going to build!
+    </p>
+  </header>
+  )
+}
+
+const App = () => {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+     <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
