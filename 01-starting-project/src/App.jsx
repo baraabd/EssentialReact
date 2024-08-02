322f8e4dcb4b6a019910c8af02a6cb956ba07975
baraabd <bara.abdullatif@outlook.com>
2024-08-02 16:13:56 +0200

    initial project

---
+++ 01-starting-project/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <h1>React Essentials For all they need to understand React</h1>
+        <p>
+          Fundamental React concepts YOU will need for almost any app you are
+          going to build!
+        </p>
+      </header>
+      <main>
+        <h2>It's Time to get started :)</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
