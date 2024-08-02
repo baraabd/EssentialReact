9a364e918ee89253cc716028a3abee1d0f98b66a
baraabd <bara.abdullatif@outlook.com>
2024-08-02 21:53:01 +0200

    Making Components Reusable with Props

--- EssentailReact/src/App.jsx
+++ EssentailReact/src/App.jsx
@@ -1,35 +1,68 @@
-import reactImg from './assets/react-core-concepts.png'
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 const genRandomInt = (max) => {
-return Math.floor(Math.random() * (max + 1))}
+  return Math.floor(Math.random() * (max + 1));
+};
 
-
+const CoreConcept = (props) => {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+};
 
 const Header = () => {
-  const description = reactDescriptions[genRandomInt(reactDescriptions.length-1)]
+  const description =
+    reactDescriptions[genRandomInt(reactDescriptions.length - 1)];
 
   return (
     <header>
-    <img src={reactImg} alt="Stylized atom" />
-    <h1>React Essentials</h1>
-    <p>
-      {description} React concepts you will need for almost any app you are
-      going to build!
-    </p>
-  </header>
-  )
-}
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+};
 
 const App = () => {
   return (
     <div>
-     <Header />
+      <Header />
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Components"
+              description="The core UI building block"
+              image={componentsImg}
+            />
+
+            <CoreConcept
+            
+            />
+
+            <CoreConcept
+              
+            />
+
+            <CoreConcept
+              
+            />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
   );
-}
+};
 
 export default App;
