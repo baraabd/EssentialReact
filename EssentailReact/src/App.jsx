4bb48b1424e2300ec1ed975772897250c009e971
baraabd <bara.abdullatif@outlook.com>
2024-08-02 20:55:24 +0200

    Dynamically loading image files

--- EssentailReact/src/App.jsx
+++ EssentailReact/src/App.jsx
@@ -1,4 +1,4 @@
-
+import reactImg from './assets/react-core-concepts.png'
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 const genRandomInt = (max) => {
@@ -8,10 +8,10 @@ return Math.floor(Math.random() * (max + 1))}
 
 const Header = () => {
   const description = reactDescriptions[genRandomInt(reactDescriptions.length-1)]
-  
+
   return (
     <header>
-    <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+    <img src={reactImg} alt="Stylized atom" />
     <h1>React Essentials</h1>
     <p>
       {description} React concepts you will need for almost any app you are
