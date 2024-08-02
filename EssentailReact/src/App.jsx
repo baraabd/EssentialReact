90df0af5e52285d64fd57f8a5dc063b36aad3eae
baraabd <bara.abdullatif@outlook.com>
2024-08-02 16:41:00 +0200

    inital project

--- EssentailReact/src/App.jsx
+++ EssentailReact/src/App.jsx
@@ -3,14 +3,14 @@ function App() {
     <div>
       <header>
         <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials For all they need to understand React</h1>
+        <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts YOU will need for almost any app you are
+          Fundamental React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>
       <main>
-        <h2>It's Time to get started :)</h2>
+        <h2>Time to get started!</h2>
       </main>
     </div>
   );
