545cd184da31e59a4c4db5ca73bf595f7e771b83
baraabd <bara.abdullatif@outlook.com>
2024-08-04 15:54:10 +0200

    Deriving & outputting data based on State

--- EssentailReact/src/App.jsx
+++ EssentailReact/src/App.jsx
@@ -3,11 +3,11 @@ import {CORE_CONCEPTS} from "./data"
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept";
 import TabButton from "./TabButton/TabButton";
-
+import { EXAMPLES } from './data.js';
 
 const App = () => {
 
-  const [selectedTopic, setSelectedTopic] = useState('Please click a button');
+  const [selectedTopic, setSelectedTopic] = useState('components');
   let tabContent = 'Please click a button';
 
 
@@ -36,12 +36,21 @@ const App = () => {
          <h2>Examples</h2>
          <menu>
           <TabButton onSelect={() => handleSelect('components')} >Components</TabButton>
-          <TabButton onSelect={() => handleSelect('JSX')}>JSX</TabButton>
-          <TabButton onSelect={() => handleSelect('Props')}>Props</TabButton>
-          <TabButton onSelect={() => handleSelect('State')}>State</TabButton>
+          <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+          <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+          <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
          </menu>
+         <div id="tab-content">
+          <h3>{EXAMPLES[selectedTopic].title}</h3>
+          <p>{EXAMPLES[selectedTopic].description}</p>
+          <pre>
+            <code>
+              {EXAMPLES[selectedTopic].code}
+            </code>
+          </pre>
+         
+         </div>
 
-         {selectedTopic}
 
         </section>
 
