3aeae9ff93ea754975966a25d9d025893272366c
baraabd <bara.abdullatif@outlook.com>
2024-08-04 18:16:29 +0200

    Rendering content conidtionally with useState

--- EssentailReact/src/App.jsx
+++ EssentailReact/src/App.jsx
@@ -1,23 +1,36 @@
 import { useState } from "react";
-import {CORE_CONCEPTS} from "./data"
+import { CORE_CONCEPTS } from "./data";
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept";
 import TabButton from "./TabButton/TabButton";
-import { EXAMPLES } from './data.js';
+import { EXAMPLES } from "./data.js";
 
 const App = () => {
 
-  const [selectedTopic, setSelectedTopic] = useState('components');
-  let tabContent = 'Please click a button';
-
+  const [selectedTopic, setSelectedTopic] = useState();
+  
 
   const handleSelect = (selectedButton) => {
-      console.log(selectedButton);
-      tabContent = selectedButton;
-      setSelectedTopic(selectedButton);
+    console.log(selectedButton);
+    tabContent = selectedButton;
+    setSelectedTopic(selectedButton);
+  };
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
   }
 
-  console.log('App Component Rendering');
+  console.log("App Component Rendering");
   return (
     <div>
       <Header />
@@ -25,33 +38,24 @@ const App = () => {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-          <CoreConcept {...CORE_CONCEPTS[0]} />
-          <CoreConcept {...CORE_CONCEPTS[1]} />
-          <CoreConcept {...CORE_CONCEPTS[2]} />
-          <CoreConcept {...CORE_CONCEPTS[3]} />
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
 
         <section id="examples">
-         <h2>Examples</h2>
-         <menu>
-          <TabButton onSelect={() => handleSelect('components')} >Components</TabButton>
-          <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-          <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-          <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
-         </menu>
-         <div id="tab-content">
-          <h3>{EXAMPLES[selectedTopic].title}</h3>
-          <p>{EXAMPLES[selectedTopic].description}</p>
-          <pre>
-            <code>
-              {EXAMPLES[selectedTopic].code}
-            </code>
-          </pre>
-         
-         </div>
-
-
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+          </menu>
+          {tabContent}
         </section>
 
         <h2>Time to get started!</h2>
